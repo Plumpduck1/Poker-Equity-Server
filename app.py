35f66a3876cde2ad00a5f74c64546a9933b9783c
Plumpduck1 <76420648+Plumpduck1@users.noreply.github.com>
2025-12-12 12:39:13 +1300

    Getting ready for server

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, os
 import random
 from equity import calculate_equity_multi
 
@@ -281,4 +281,5 @@ def game_state_json():
 # -----------------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
